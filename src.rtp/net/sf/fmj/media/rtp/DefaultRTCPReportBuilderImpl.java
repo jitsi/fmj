91f39c8a7964a32224036524cfa66c0d5a023f50
gpolitis2014 <gpolitis2014@799b4810-d841-4bb4-a54b-9156a0c3e4cd>
2014-07-15 14:10:39 +0000

    Makes it possible to override the default RTCP report creation algorithm by introducing a new interface called RTCPReportBuilder.
    git-svn-id: svn://svn.code.sf.net/p/fmj/code/fmj@51 799b4810-d841-4bb4-a54b-9156a0c3e4cd

---
+++ src.rtp/net/sf/fmj/media/rtp/DefaultRTCPReportBuilderImpl.java
@@ -0,0 +1,150 @@
+package net.sf.fmj.media.rtp;
+
+import java.util.*;
+
+/**
+ * Created by gp on 6/12/14.
+ */
+public class DefaultRTCPReportBuilderImpl implements RTCPReportBuilder
+{
+    private int sdescounter;
+    private RTCPTransmitter rtcpTransmitter;
+
+    public DefaultRTCPReportBuilderImpl()
+    {
+        reset();
+    }
+
+    @Override
+    public RTCPPacket[] makeReports()
+    {
+        if (rtcpTransmitter == null)
+            throw new IllegalStateException("rtcpTransmitter is not set");
+
+        Vector<RTCPPacket> packets = new Vector<RTCPPacket>();
+        SSRCInfo ourinfo = rtcpTransmitter.ssrcInfo;
+        boolean senderreport = ourinfo.sender;
+        long time = System.currentTimeMillis();
+        RTCPReportBlock reports[] = makeReceiverReports(time);
+        RTCPReportBlock firstrep[] = reports;
+        if (reports.length > 31)
+        {
+            firstrep = new RTCPReportBlock[31];
+            System.arraycopy(reports, 0, firstrep, 0, 31);
+        }
+        if (senderreport)
+        {
+            RTCPSRPacket srp = new RTCPSRPacket(ourinfo.ssrc, firstrep);
+            packets.addElement(srp);
+            long systime = ourinfo.systime == 0L ? System.currentTimeMillis()
+                    : ourinfo.systime;
+            long secs = systime / 1000L;
+            double msecs = (systime - secs * 1000L) / 1000D;
+            srp.ntptimestamplsw = (int) (msecs * 4294967296D);
+            srp.ntptimestampmsw = secs;
+            srp.rtptimestamp = (int) ourinfo.rtptime;
+            srp.packetcount = ourinfo.maxseq - ourinfo.baseseq;
+            srp.octetcount = ourinfo.bytesreceived;
+        } else
+        {
+            RTCPRRPacket rrp = new RTCPRRPacket(ourinfo.ssrc, firstrep);
+            packets.addElement(rrp);
+        }
+        if (firstrep != reports)
+        {
+            for (int offset = 31; offset < reports.length; offset += 31)
+            {
+                if (reports.length - offset < 31)
+                    firstrep = new RTCPReportBlock[reports.length - offset];
+                System.arraycopy(reports, offset, firstrep, 0, firstrep.length);
+                RTCPRRPacket rrp = new RTCPRRPacket(ourinfo.ssrc, firstrep);
+                packets.addElement(rrp);
+            }
+
+        }
+        RTCPSDESPacket sp = new RTCPSDESPacket(new RTCPSDES[1]);
+        sp.sdes[0] = new RTCPSDES();
+        sp.sdes[0].ssrc = rtcpTransmitter.ssrcInfo.ssrc;
+        Vector<RTCPSDESItem> itemvec = new Vector<RTCPSDESItem>();
+        itemvec.addElement(new RTCPSDESItem(1, ourinfo.sourceInfo.getCNAME()));
+        if (sdescounter % 3 == 0)
+        {
+            if (ourinfo.name != null && ourinfo.name.getDescription() != null)
+                itemvec.addElement(new RTCPSDESItem(2, ourinfo.name
+                        .getDescription()));
+            if (ourinfo.email != null && ourinfo.email.getDescription() != null)
+                itemvec.addElement(new RTCPSDESItem(3, ourinfo.email
+                        .getDescription()));
+            if (ourinfo.phone != null && ourinfo.phone.getDescription() != null)
+                itemvec.addElement(new RTCPSDESItem(4, ourinfo.phone
+                        .getDescription()));
+            if (ourinfo.loc != null && ourinfo.loc.getDescription() != null)
+                itemvec.addElement(new RTCPSDESItem(5, ourinfo.loc
+                        .getDescription()));
+            if (ourinfo.tool != null && ourinfo.tool.getDescription() != null)
+                itemvec.addElement(new RTCPSDESItem(6, ourinfo.tool
+                        .getDescription()));
+            if (ourinfo.note != null && ourinfo.note.getDescription() != null)
+                itemvec.addElement(new RTCPSDESItem(7, ourinfo.note
+                        .getDescription()));
+        }
+        sdescounter++;
+        sp.sdes[0].items = new RTCPSDESItem[itemvec.size()];
+        itemvec.copyInto(sp.sdes[0].items);
+        packets.addElement(sp);
+
+        // Copy into an array and return.
+        RTCPPacket[] res = new RTCPPacket[packets.size()];
+        packets.copyInto(res);
+        return res;
+    }
+
+    private RTCPReportBlock[] makeReceiverReports(long time)
+    {
+        Vector<RTCPReportBlock> reports = new Vector<RTCPReportBlock>();
+
+        // Make receiver reports for all known SSRCs.
+        for (Enumeration<SSRCInfo> elements = rtcpTransmitter.cache.cache.elements();
+             elements.hasMoreElements();)
+        {
+            SSRCInfo info = elements.nextElement();
+            if (!info.ours && info.sender)
+            {
+                RTCPReportBlock receiverReport = new RTCPReportBlock();
+                receiverReport.ssrc = info.ssrc;
+                receiverReport.lastseq = info.maxseq + info.cycles;
+                receiverReport.jitter = (int) info.jitter;
+                receiverReport.lsr = (int) ((info.lastSRntptimestamp & 0x0000ffffffff0000L) >> 16);
+                receiverReport.dlsr = (int) ((time - info.lastSRreceiptTime) * 65.536000000000001D);
+                receiverReport.packetslost = (int) (((receiverReport.lastseq - info.baseseq) + 1L) - info.received);
+                if (receiverReport.packetslost < 0)
+                    receiverReport.packetslost = 0;
+                double frac = (double) (receiverReport.packetslost - info.prevlost)
+                        / (double) (receiverReport.lastseq - info.prevmaxseq);
+                if (frac < 0.0D)
+                    frac = 0.0D;
+                receiverReport.fractionlost = (int) (frac * 256D);
+                info.prevmaxseq = (int) receiverReport.lastseq;
+                info.prevlost = receiverReport.packetslost;
+                reports.addElement(receiverReport);
+            }
+        }
+
+        // Copy into an array and return.
+        RTCPReportBlock res[] = new RTCPReportBlock[reports.size()];
+        reports.copyInto(res);
+        return res;
+    }
+
+    @Override
+    public void reset()
+    {
+        sdescounter = 0;
+    }
+
+    @Override
+    public void setRTCPTransmitter(RTCPTransmitter rtcpTransmitter)
+    {
+        this.rtcpTransmitter = rtcpTransmitter;
+    }
+}
--- src.rtp/net/sf/fmj/media/rtp/RTCPTransmitter.java
+++ src.rtp/net/sf/fmj/media/rtp/RTCPTransmitter.java
@@ -10,14 +10,13 @@ public class RTCPTransmitter
 {
     RTCPRawSender sender;
     OverallStats stats;
-    SSRCCache cache;
-    int sdescounter;
-    SSRCInfo ssrcInfo;
+    public SSRCCache cache;
+    RTCPReportBuilder reportBuilder;
+    public SSRCInfo ssrcInfo;
 
     public RTCPTransmitter(SSRCCache cache)
     {
         stats = null;
-        sdescounter = 0;
         ssrcInfo = null;
         this.cache = cache;
         stats = cache.sm.defaultstats;
@@ -47,9 +46,19 @@ public class RTCPTransmitter
         if (!cache.rtcpsent)
             return;
         cache.byestate = true;
-        Vector repvec = makereports();
-        RTCPPacket packets[] = new RTCPPacket[repvec.size() + 1];
-        repvec.copyInto(packets);
+
+        RTCPReportBuilder rb = getReportBuilder();
+        RTCPPacket reports[] = rb.makeReports();
+
+        RTCPPacket packets[];
+        if (reports == null || reports.length == 0)
+            packets = new RTCPPacket[1];
+        else
+        {
+            packets = new RTCPPacket[reports.length + 1];
+            System.arraycopy(reports, 0, packets, 0, reports.length);
+        }
+
         int ssrclist[] = new int[1];
         ssrclist[0] = ssrc;
         RTCPBYEPacket byep = new RTCPBYEPacket(ssrclist, reason);
@@ -72,7 +81,7 @@ public class RTCPTransmitter
             e.printStackTrace();
         }
         transmit(cp);
-        sdescounter = 0;
+        rb.reset();
     }
 
     public void bye(String reason)
@@ -94,121 +103,11 @@ public class RTCPTransmitter
         return sender;
     }
 
-    protected RTCPReportBlock[] makerecreports(long time)
-    {
-        Vector<RTCPReportBlock> reports = new Vector<RTCPReportBlock>();
-        for (Enumeration<SSRCInfo> elements = cache.cache.elements();
-                elements.hasMoreElements();)
-        {
-            SSRCInfo info = elements.nextElement();
-            if (!info.ours && info.sender)
-            {
-                RTCPReportBlock rep = new RTCPReportBlock();
-                rep.ssrc = info.ssrc;
-                rep.lastseq = info.maxseq + info.cycles;
-                rep.jitter = (int) info.jitter;
-                rep.lsr = (int) ((info.lastSRntptimestamp & 0x0000ffffffff0000L) >> 16);
-                rep.dlsr = (int) ((time - info.lastSRreceiptTime) * 65.536000000000001D);
-                rep.packetslost = (int) (((rep.lastseq - info.baseseq) + 1L) - info.received);
-                if (rep.packetslost < 0)
-                    rep.packetslost = 0;
-                double frac = (double) (rep.packetslost - info.prevlost)
-                        / (double) (rep.lastseq - info.prevmaxseq);
-                if (frac < 0.0D)
-                    frac = 0.0D;
-                rep.fractionlost = (int) (frac * 256D);
-                info.prevmaxseq = (int) rep.lastseq;
-                info.prevlost = rep.packetslost;
-                reports.addElement(rep);
-            }
-        }
-
-        RTCPReportBlock reportsarr[] = new RTCPReportBlock[reports.size()];
-        reports.copyInto(reportsarr);
-        return reportsarr;
-    }
-
-    protected Vector makereports()
-    {
-        Vector packets = new Vector();
-        SSRCInfo ourinfo = ssrcInfo;
-        boolean senderreport = ourinfo.sender;
-        long time = System.currentTimeMillis();
-        RTCPReportBlock reports[] = makerecreports(time);
-        RTCPReportBlock firstrep[] = reports;
-        if (reports.length > 31)
-        {
-            firstrep = new RTCPReportBlock[31];
-            System.arraycopy(reports, 0, firstrep, 0, 31);
-        }
-        if (senderreport)
-        {
-            RTCPSRPacket srp = new RTCPSRPacket(ourinfo.ssrc, firstrep);
-            packets.addElement(srp);
-            long systime = ourinfo.systime == 0L ? System.currentTimeMillis()
-                    : ourinfo.systime;
-            long secs = systime / 1000L;
-            double msecs = (systime - secs * 1000L) / 1000D;
-            srp.ntptimestamplsw = (int) (msecs * 4294967296D);
-            srp.ntptimestampmsw = secs;
-            srp.rtptimestamp = (int) ourinfo.rtptime;
-            srp.packetcount = ourinfo.maxseq - ourinfo.baseseq;
-            srp.octetcount = ourinfo.bytesreceived;
-        } else
-        {
-            RTCPRRPacket rrp = new RTCPRRPacket(ourinfo.ssrc, firstrep);
-            packets.addElement(rrp);
-        }
-        if (firstrep != reports)
-        {
-            for (int offset = 31; offset < reports.length; offset += 31)
-            {
-                if (reports.length - offset < 31)
-                    firstrep = new RTCPReportBlock[reports.length - offset];
-                System.arraycopy(reports, offset, firstrep, 0, firstrep.length);
-                RTCPRRPacket rrp = new RTCPRRPacket(ourinfo.ssrc, firstrep);
-                packets.addElement(rrp);
-            }
-
-        }
-        RTCPSDESPacket sp = new RTCPSDESPacket(new RTCPSDES[1]);
-        sp.sdes[0] = new RTCPSDES();
-        sp.sdes[0].ssrc = ssrcInfo.ssrc;
-        Vector<RTCPSDESItem> itemvec = new Vector<RTCPSDESItem>();
-        itemvec.addElement(new RTCPSDESItem(1, ourinfo.sourceInfo.getCNAME()));
-        if (sdescounter % 3 == 0)
-        {
-            if (ourinfo.name != null && ourinfo.name.getDescription() != null)
-                itemvec.addElement(new RTCPSDESItem(2, ourinfo.name
-                        .getDescription()));
-            if (ourinfo.email != null && ourinfo.email.getDescription() != null)
-                itemvec.addElement(new RTCPSDESItem(3, ourinfo.email
-                        .getDescription()));
-            if (ourinfo.phone != null && ourinfo.phone.getDescription() != null)
-                itemvec.addElement(new RTCPSDESItem(4, ourinfo.phone
-                        .getDescription()));
-            if (ourinfo.loc != null && ourinfo.loc.getDescription() != null)
-                itemvec.addElement(new RTCPSDESItem(5, ourinfo.loc
-                        .getDescription()));
-            if (ourinfo.tool != null && ourinfo.tool.getDescription() != null)
-                itemvec.addElement(new RTCPSDESItem(6, ourinfo.tool
-                        .getDescription()));
-            if (ourinfo.note != null && ourinfo.note.getDescription() != null)
-                itemvec.addElement(new RTCPSDESItem(7, ourinfo.note
-                        .getDescription()));
-        }
-        sdescounter++;
-        sp.sdes[0].items = new RTCPSDESItem[itemvec.size()];
-        itemvec.copyInto(sp.sdes[0].items);
-        packets.addElement(sp);
-        return packets;
-    }
-
     public void report()
     {
-        Vector repvec = makereports();
-        RTCPPacket packets[] = new RTCPPacket[repvec.size()];
-        repvec.copyInto(packets);
+        RTCPPacket packets[] = getReportBuilder().makeReports();
+        if (packets == null || packets.length == 0)
+            return;
         RTCPCompoundPacket cp = new RTCPCompoundPacket(packets);
         transmit(cp);
     }
@@ -243,5 +142,20 @@ public class RTCPTransmitter
             stats.update(OverallStats.TRANSMITFAILED, 1);
             cache.sm.transstats.transmit_failed++;
         }
+    }
+
+    public void setReportBuilder(
+            RTCPReportBuilder reportBuilder)
+    {
+        this.reportBuilder = reportBuilder;
+        this.reportBuilder.setRTCPTransmitter(this);
+    }
+
+    private RTCPReportBuilder getReportBuilder()
+    {
+        if (reportBuilder == null)
+            setReportBuilder(new DefaultRTCPReportBuilderImpl());
+
+        return reportBuilder;
     }
 }
